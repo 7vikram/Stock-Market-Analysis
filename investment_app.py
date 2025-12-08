d1b84ebc1247d9259d6d4e53db44558064b8179f
Vikram Purohit <43677894+7vikram@users.noreply.github.com>
2025-12-08 20:18:57 +0530

    Update investment_app.py

--- investment_app.py
+++ investment_app.py
@@ -53,8 +53,8 @@ with tabs[0]:
 
     # User Inputs
     etf_ticker = st.sidebar.selectbox('Select ETF Ticker', etf_tickers)
-    start_date = st.sidebar.date_input('Start Date', pd.to_datetime("today"))
-    end_date = st.sidebar.date_input('End Date', pd.to_datetime("today") - pd.DateOffset(years=1))
+    start_date = st.sidebar.date_input('Start Date', pd.to_datetime("today") - pd.DateOffset(years=1))
+    end_date = st.sidebar.date_input('End Date', pd.to_datetime("today"))
     max_investment = st.sidebar.number_input('Max Investment (₹)', value=200000, step=1000)
     monthly_investment = st.sidebar.number_input('Monthly Investment (₹)', value=20000, step=1000)
     market_drop_threshold = st.sidebar.slider('Market Drop Threshold', min_value=1, max_value=20, value=3, step=1)
@@ -173,8 +173,8 @@ with tabs[1]:
     # User inputs for tickers and date range
     ticker1 = st.selectbox("Select First Ticker", etf_tickers, index=0)
     ticker2 = st.selectbox("Select Second Ticker", etf_tickers, index=1)
-    chart_start_date = st.date_input("Chart Start Date", pd.to_datetime("today"))
-    chart_end_date = st.date_input("Chart End Date", pd.to_datetime("today") - pd.DateOffset(years=1))
+    chart_start_date = st.date_input("Chart Start Date", pd.to_datetime("today") - pd.DateOffset(years=1))
+    chart_end_date = st.date_input("Chart End Date", pd.to_datetime("today"))
 
     if st.button("Plot Chart"):
         # Fetch data for both tickers
